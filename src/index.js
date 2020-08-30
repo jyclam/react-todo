6580f2220a86a1e0e419b57bd255834dabfc025d
John <john.dev.work.01@gmail.com>
2020-08-30 16:27:14 -0700

    Add prettier, add various packages to customize babel config without ejecting CRA

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "antd/dist/antd.css";
+import GlobalStyles from "./GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
-
