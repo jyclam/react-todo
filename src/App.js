a949776d9da21bb8cf8cf3abb6cddce7d8906491
John <john.dev.work.01@gmail.com>
2020-08-30 20:54:28 -0700

    Add layout and structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout } from "antd";
+
+import SideMenu from "./components/Menu";
+import Content from "./components/Content";
+
+const { Sider } = Layout;
+
+const layoutStyles = { height: "100vh" };
+const triggerStyles = { top: "60%" };
 
 function App() {
+  const [collapse, setCollapse] = useState(true);
+
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    <Layout style={layoutStyles}>
+      <Sider
+        onClick={() => setCollapse(!collapse)}
+        collapsible
+        collapsed={collapse}
+        collapsedWidth="0"
+        width="100"
+        zeroWidthTriggerStyle={triggerStyles}
+      >
+        <SideMenu />
+      </Sider>
+      <Content />
+    </Layout>
   );
 }
 
